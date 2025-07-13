cb1528b023a54815849b55b0059e5c30fe6eb500
Sergeev Sergey <Sergeev_sergey99@mail.ru>
2025-07-13 23:24:48 +0300

    Измененный спавн

---
+++ Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    [SerializeField] private PipeController pipePrefab;
+    [SerializeField] private float spawnInterval = 2f;
+    
+    private float timer;
+    private void Start()
+    {
+        timer = spawnInterval;
+    }
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0f)
+        {
+            SpawnPipe();
+            timer = spawnInterval;
+        }
+    }
+    private void SpawnPipe()
+    {
+        float randomY = Random.Range(-2f, 2f);
+        Vector2 spawnPosition = new Vector2(10f, randomY);
+        Instantiate(pipePrefab, spawnPosition, Quaternion.identity);
+    }
+}
